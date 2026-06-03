79246898c0d6bc8a4c858e3e83a6737be525d24e
lahavsliman <lahavdigital@gmail.com>
2026-06-03 13:37:49 +0300

    add google tag

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,22 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+
+        {/* Google tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1TM6SQBV6X"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-1TM6SQBV6X');
+            `,
+          }}
+        />
       </Head>
+
       <body>
         <Main />
         <NextScript />
